56ff03fc1c7acd22abf791a42bf702705a537ff9
Dmitriy-M1319 <dimon.5642014@yandex.ru>
2023-05-21 15:57:57 +0300

    fix settings and run.sh

--- TestJenkins/run.sh
+++ TestJenkins/run.sh
@@ -2,4 +2,4 @@
 
 python ./manage.py migrate
 python ./manage.py collectstatic --noinput
-gunicorn --forwarded-allow-ips=* --bind 0.0.0.0:8080 -w 2 app.wsgi:application
+gunicorn --forwarded-allow-ips=* --bind 0.0.0.0:8080 -w 2 TestJenkins.wsgi:application
