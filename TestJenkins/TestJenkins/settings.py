ed48b8e2dae945c84445d4a3603e5b41608673e0
Dmitriy Mamonov <dimon.5642014@yandex.ru>
2023-05-21 16:32:21 +0300

    refactor static file

--- TestJenkins/TestJenkins/settings.py
+++ TestJenkins/TestJenkins/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, '..', "frontend", "src"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'public', 'media')
 MEDIA_URL = '/media/'
